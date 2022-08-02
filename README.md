cd6421e9e36c745b94d9099adb3dd9c5522b71c1
Your Name <you@example.com>
2022-08-02 13:23:52 -0400

    Fixed Views funciontalities + tested URLs

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Products_API
+
+As a developer, I want my API to serve content on the following urls paths:
+Paths must match these exactly!
+· ‘127.0.0.1:8000/api/products/'
+· ‘127.0.0.1:8000/api/products/<int:pk>/’
+
+As a developer, I want to build a REST web API in Django REST Framework, so that I can make HTTP requests interact with the data set.
+
+BONUS
+
+(/5 points) As a developer, I want to add the ability to add an image link to each product. (Link to picture on the internet, this will just be a simple CharField representing the URL of the image, you do NOT need to add an actual image file.) --> -->
--- products/views.py
+++ products/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -25,14 +23,15 @@ from .serializers import ProductSerializer
 @api_view (['GET', 'POST'])
 def product_list(request):
     if request.method == 'GET':
-        products = Product.objects.all()
+        product = Product.objects.all()
     elif request.method == 'POST':
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    serializer = ProductSerializer(products, many=True)
+    serializer = ProductSerializer(product, many=True)
+
     return Response(serializer.data)
 
 # As a developer, I want to create a PUT endpoint that does the following things:
@@ -53,9 +52,10 @@ def product_detail(request, pk):
         serializer = ProductSerializer(product);
         return Response(serializer.data)
     elif request.method == 'PUT':
-        serializer = ProductSerializer(Product, data=request.data)
+        serializer = ProductSerializer(product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        return Response(serializer.data)
     elif request.method == 'DELETE':
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
